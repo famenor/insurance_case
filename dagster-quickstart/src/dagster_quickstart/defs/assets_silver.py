39dfafaa72e5ae880854d8d762ecdede502eb0ae
famenor <armando_859941@yahoo.com.mx>
2025-11-05 18:01:09 -0600

    refactor

--- dagster-quickstart/src/dagster_quickstart/defs/assets_silver.py
+++ dagster-quickstart/src/dagster_quickstart/defs/assets_silver.py
@@ -474,13 +474,13 @@ def generate_insurance_tables():
     br_claim = extract_claims(sl_cert, sl_cie)
     load_claim_consultation(br_claim)
 
-from dagster import AssetSelection, define_asset_job
+#from dagster import AssetSelection, define_asset_job
 
 ## JOB FOR GOLD TABLES
-generate_gold_tables = define_asset_job(
-    name="generate_gold_tables",
-    selection=AssetSelection.assets("report_customer_interaction", "report_age_at_diagnosis")
-)
+#generate_gold_tables = define_asset_job(
+#    name="generate_gold_tables",
+#    selection=AssetSelection.assets("report_customer_interaction", "report_age_at_diagnosis")
+#)
 
 
 
